0c9bc84dbe223906e8c19f8182dfda34fb6b3540
Carolina Lopes <carolcandela006@gmail.com>
2025-02-17 09:18:51 -0300

    Aula sobre estrutura e códigos básicos em java

---
+++ aula01/Numeros.java
@@ -0,0 +1,10 @@
+
+public class Numeros 
+    {
+    public static void main(String[] args) {
+        System.out.println(3 + 4);
+        System.out.println(3 - 4);
+        System.out.println(3 * 4);
+        System.out.println(9 / 3);
+    }
+}
