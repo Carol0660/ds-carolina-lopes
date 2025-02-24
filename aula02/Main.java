2c982917798630baa1be759254c5cdf0374cd240
Carolina Lopes <carolcandela006@gmail.com>
2025-02-24 09:31:12 -0300

    Aula de Java

---
+++ aula02/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {    
+    
+    public static void main(String[] args) {
+      String msg  = "Ola, tudo bom?";
+      System.out.println(msg);
+      int n1 = 3;
+      int n2 = 4;
+      float n3 = 1.88f;
+      System.out.println(n1);
+      System.out.println(n1 + n2);
+      System.out.println(n1 + n2 + n3);
+
+    }
+  }
+  
