40220622008506ba9e33a2781444d6c967403619
Carolina Lopes <carolcandela006@gmail.com>
2025-03-10 09:20:49 -0300

    Regras de tipagem e imput

---
+++ aula03/TesteConversao.java
@@ -0,0 +1,7 @@
+
+public class TesteConversao {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ aula03/TiposVariaveis.java
@@ -0,0 +1,15 @@
+
+public class TiposVariaveis {
+        //Calculadora de salário
+        public static void main(String[] args) {
+          int horas = 5;               
+          float valorhora = 15.75f;    
+          String real = "R$"; 
+          float total = valorhora* horas;              
+          System.out.println("Horas trabalhadas: " + horas);
+          System.out.println("Valor de 1 hora:" + real + valorhora);
+          System.out.println("Salário = " + real + total);
+         
+        }  
+        
+}
