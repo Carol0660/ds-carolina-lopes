f785771df367d05be03dac2dbd713abb7eca02c8
unknown <carolcandela006@gmail.com>
2025-03-17 09:31:17 -0300

    Aula sobre condições no java

---
+++ aula04/Condicao.java
@@ -0,0 +1,12 @@
+public class Condicao {
+    public static void main(String[] args) {
+        int V1 = 12;
+        int V2 = 16;
+      if (V1 > V2) {
+        System.out.println(V1 + " e maior que" + V2); 
+      }  else{
+        System.out.println(V1 + " e menor que " + V2);
+      }
+
+    }
+  }
