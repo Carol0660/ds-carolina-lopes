5153c14de3518713aa219d534b4017a74bd42ff9
Carolina Lopes <carolcandela006@gmail.com>
2025-03-30 15:27:33 -0300

    Exercicios em Java

--- Exercicios/Exercicio01.java
+++ Exercicios/Exercicio01.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 
 public class Exercicio01 {
      public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
-    String nome;
     
-    System.out.println("Escreva o nome do funcionario");
+        Scanner myObj = new Scanner(System.in);
+    
+    System.out.println ("Escreva o nome do funcionario");
     String nome = myObj.nextLine();  
     
     System.out.println("Escreva o numero de horas trabalhadas: ");
     int v1 = myObj.nextInt();
 
     System.out.println("Escreva o valor recebido por hora trabalhada: ");
-    int v2 = myObj.nextInt();
+    float v2 = myObj.nextFloat();
 
     System.out.println("Escreva quantos filhos o funcionario tem menores de 14 anos: ");
     int v3 = myObj.nextInt();
@@ -21,38 +21,46 @@ public class Exercicio01 {
     System.out.println("Escreva a idade do funcionario: ");
     int v4 = myObj.nextInt();
 
-    System.out.println("Escreva o valor do salário família por filho: ");
-    int v5 = myObj.nextInt();
-
     System.out.println("Tempo de serviço em anos: ");
     int v6 = myObj.nextInt();
     
     float salBruto = v1* v2;
-    double descont1 = (salBruto * 8.5) / 100;
+    double descont = (salBruto * 8.5) / 100;
     float salFami = v3* 65;
-
-
-    String real = "R$"; 
+    float ir = 0;
+    double adic;
 
     myObj.close();
 
-    if (salBruto > 1500);{
-        float ir1 = (salBruto* 15) / 100;
+    if (salBruto > 1500){
+        ir = (salBruto* 15) / 100;
     }
-    else if (500 < salBruto){
-        float ir2 = salBruto / 8*100;
+    else if (salBruto > 500){
+        ir = (salBruto* 8) / 100;
     }
     else if (salBruto <= 500){
-        System.out.println("IR é: 0");
+        ir = 0;
     }
 
     if (v4 > 40){
-        float adic = (salBruto* 2) / 100;
+        adic = (salBruto* 2) / 100;
     }
     else if (v6 > 15){
-        double adic2 = (salBruto* 3.5) / 100;
+        adic = (salBruto* 3.5) / 100;
     }
-    else if (v6 <= )
-
+    else if (v6 <= 15 && v6 > 5 && v4 > 30){
+        adic = (salBruto* 1.5) / 100;
     }
+    else {
+        adic = 0;
+    }
+    double salLiquido = salBruto + adic + salFami - descont - ir;
+    double descontos = ir + descont;
+    System.out.println("Nome do funcionario: " + nome);
+    System.out.println("Salario Bruto: R$" + salBruto);
+    System.out.println("Descontos: R$" + descontos);
+    System.out.println("Adicionais: R$" + adic);
+    System.out.println("Salario liquido: R$" + salLiquido);
+    
+}
 }
--- Exercicios/Exercicio02.java
+++ Exercicios/Exercicio02.java
@@ -1,11 +1,37 @@
-package Exercicios;
-
 import java.util.Scanner;
 
 public class Exercicio02 {
-    public static void main(String[] args){
-        Scanner meuScan = new Scanner(System.in);
+        public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);
         System.out.println("Escreva o codigo do funcionario: ");
+        String cod = myObj.nextLine();
+        System.out.println("Digite F se seu sexo for feminino e M para o caso de ser masculino: ");
+        String sexo = myObj.nextLine();
+        System.out.println("Escreva quantos anos o funcionario trabalha na empresa: ");
+        int v1 = myObj.nextInt();
+        System.out.println("Escreva o salario do funcionario: ");
+        float v2 = myObj.nextFloat();
+        float bonus = 0;
+        
+
+
+        myObj.close();
+
+        if (sexo == ("M") && v1 > 15){
+            bonus = (v2 * 20) / 100;
+        }
+        else if (sexo == ("F") && v1 > 10){
+            bonus = (v2 * 25) / 100;
+        }
+        else if (sexo != ("M") && sexo != ("F")) {
+            System.out.println("Sexo invalido");
+        }
+        else {
+            bonus = 100;
+        }
+
+        System.out.println("O bonus sera: R$" + bonus);
+        
         
 
     }
